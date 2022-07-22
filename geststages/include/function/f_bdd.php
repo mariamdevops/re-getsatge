e093f103d6708ba249f219583a2826e14a838bb6
mariamdevops <109662486+mariamdevops@users.noreply.github.com>
2022-07-22 11:31:31 +0200

    Add files via upload

---
+++ geststages/include/function/f_bdd.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+function connexionBDD()
+{
+	try
+	{
+            $bdd = new PDO('mysql:host=server-base.mariadb.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 'vhugo@server-base', 'P@ssw0rd', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            return $bdd;
+	}
+	catch(Exception $e)
+	{
+		$pdo_error = $e->getMessage();
+                return false;
+	}
+    
+}
+
+?>
